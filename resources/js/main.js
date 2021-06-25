bf142a7f0a40c6a3f048db1eeabb8811e980cdb6
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2021-06-25 22:38:08 +0600

    nav

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
+
+  // sticky nav
+    $(".js--services-section").waypoint(function(direction){
+      
+        if(direction == "down"){
+            $("nav").addClass("sticky");
+
+        }
+        else{
+            $("nav").removeClass("sticky");
+
+        }
+    });
+    // mixit up portfolio
     var mixer = mixitup('.container');
 })
+
+
+
